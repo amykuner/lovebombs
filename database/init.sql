70337689addcb8b84090192a1b43de0304d6bf03
maryamgh1988 <ghorbani.maryam6688@gmail.com>
2021-04-21 16:58:45 +0100

    creat tables
    Relates #5

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,45 @@
 -- Project database schema
+BEGIN;
+DROP TABLE IF EXISTS users, fac_members, compliments CASCADE;
+
+CREATE TABLE fac_members (
+  id serial primary key,
+  full_name varchar(255) NOT NULL, 
+  img_url  text,
+  cohort_name varchar(255),
+  fac_role varchar(255)
+);
+
+CREATE TABLE users (
+  id serial primary key,
+  username varchar(255) NOT NULL
+);
+
+
+
+
+CREATE TABLE compliments (
+  id serial primary key,
+  user_id  integer References users(id) ON DELETE CASCADE,
+  fac_member_id  integer References fac_members(id) ON DELETE CASCADE,
+ text_content text,
+  created_at timestamp
+);
+
+
+INSERT INTO fac_members (full_name, img_url, cohort_name, fac_role ) VALUES
+  ('Amy', 'https://avatars.githubusercontent.com/u/72794385?v=4', 'fac21', 'student'),
+  ('Antonio', 'https://avatars.githubusercontent.com/u/63958540?v=4', 'fac21', 'student'),
+  ('Mariya', 'https://avatars.githubusercontent.com/u/72445011?v=4', 'fac21', 'student'),
+  ('Neville', 'https://avatars.githubusercontent.com/u/60395899?v=4', 'fac21', 'student')
+
+;
+
+
+INSERT INTO compliments ( fac_member_id, text_content) VALUES
+  (1, 'Amy is funny'),
+  (2, 'Antonio is smart')
+;
+
+
+COMMIT;
