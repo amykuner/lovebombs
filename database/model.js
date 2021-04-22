e996d935e0abe9cd09ec448193f68fb564b5911f
Safia <safiaali@hotmail.com>
2021-04-22 17:29:28 +0100

    add addComments function to post.js (paramertised query)

--- database/model.js
+++ database/model.js
@@ -14,10 +14,19 @@ function getFacMemberDetails(name) {
 function getComments(name) {
   return db
     .query(
-      `SELECT compliments.text_content, compliments.created_at, compliments.fac_member_id ,users.username FROM compliments INNER JOIN users ON users.id = compliments.user_id 
+      `SELECT compliments.text_content, compliments.created_at, compliments.fac_member_id ,users.username FROM compliments INNER JOIN users ON users.id = compliments.user_id
       INNER JOIN (SELECT * FROM fac_members WHERE full_name='${name}') AS member_name ON compliments.fac_member_id = member_name.id;`
     )
     .then((result) => result.rows);
 }
 
-module.exports = { getFacMembers, getFacMemberDetails, getComments};
+function addComments(values) {
+    return db
+        .query(
+            `INSERT INTO compliments (user_id, fac_member_id, text_content, created_at)
+            VALUES ($1, $2, $3, $4)`, [values]);
+}
+
+
+
+module.exports = { getFacMembers, getFacMemberDetails, getComments, addComments};
